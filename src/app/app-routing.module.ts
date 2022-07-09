90ab0c469931ff0d4b5ce97df01e5c6ff21782bb
Hugo Ramalho <ramalho.hg@gmail.com>
2022-07-09 12:52:42 -0300

    Desafio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Feature1Component } from './feature1/feature1.component';
+import { Feature2Component } from './feature2/feature2.component';
+import { Feature3Component } from './feature3/feature3.component';
+import { UtilizadorComponent } from './utilizador/utilizador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'utilizador', component: UtilizadorComponent},
+  { path: 'feature1', component: Feature1Component},
+  { path: 'feature2', component: Feature2Component},
+  { path: 'feature3', component: Feature3Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UtilizadorComponent } from './utilizador/utilizador.component';
+import { Feature1Component } from './feature1/feature1.component';
+import { Feature2Component } from './feature2/feature2.component';
+import { Feature3Component } from './feature3/feature3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilizadorComponent,
+    Feature1Component,
+    Feature2Component,
+    Feature3Component
   ],
   imports: [
     BrowserModule,
